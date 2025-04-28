ea73ce4985d36f99c09b7847bad3493806b0cf19
Vadim Kovalyov <vadimkovalyov06@gmail.com>
2025-04-28 13:02:41 +0300

    Added tools for window

--- main.py
+++ main.py
@@ -4,3 +4,17 @@ from PIL import Image, ImageTk
 
 
 access_key = "zcM-4apdjE3GtYjE3g2MA3cyORU_Pntf5CD9OFCvNfI"
+
+
+
+root = Tk()
+root.title("Random Bird")
+root.geometry("500x500")
+
+label = Label(root)
+label.pack(padx=20, pady=20)
+
+btn = Button(root, text="Next")
+btn.pack(pady=10)
+
+root.mainloop()
